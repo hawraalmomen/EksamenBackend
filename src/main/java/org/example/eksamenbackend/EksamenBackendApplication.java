d4a9427e6f10ae767624de2ac2c19bd4c0ee7aa2
Hawra Al-Momen <haal0006@stud.kea.dk>
2026-05-13 01:10:01 +0200

    Initial commit

---
+++ src/main/java/org/example/eksamenbackend/EksamenBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.eksamenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EksamenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/eksamenbackend/EksamenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eksamenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

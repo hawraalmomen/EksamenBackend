3bc731ad571926a1c80a563315bdae164e14cdcb
Hawra Al-Momen <haal0006@stud.kea.dk>
2026-05-13 09:34:58 +0200

    backend

--- src/test/java/org/example/eksamenbackend/EksamenBackendApplicationTests.java
+++ src/test/java/org/example/eksamenbackend/EksamenBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EksamenBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
